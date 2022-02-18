cec56001788072c103d84bed5a41c0f9751f2fb2
GalSanyi <galsanyi84@gmail.com>
2022-02-18 20:43:48 +0100

    add file

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,29 +1,40 @@
 import PropTypes from 'prop-types';
 import s from './FriendList.module.css';
 
-function FriendList({friends }) { 
-    return <div>
-        <ul className={s.list}>
-            {friends.map(friend =>
-                <li key={friend.id } className={ s.item}>
-                    <span
-                        className={s.status}
-                              style={
-          friend.isOnline ? { backgroundColor: 'green' } : { backgroundColor: 'red' }
-        }
-                    >{friends.isOnline}</span>
-                    <img classNname={s.avatar} src={ friend.avatar } alt="User avatar" width="48" />
-                    <p className={s.name}>{ friend.name}</p>
-</li>)}
-
-</ul>
+function FriendList({ friends }) {
+  return (
+    <div>
+      <ul className={s.list}>
+        {friends.map(friend => (
+          <li key={friend.id} className={s.item}>
+            <span
+              className={s.status}
+              style={
+                friend.isOnline
+                  ? { backgroundColor: 'green' }
+                  : { backgroundColor: 'red' }
+              }
+            >
+              {friends.isOnline}
+            </span>
+            <img
+              classNname={s.avatar}
+              src={friend.avatar}
+              alt="User avatar"
+              width="48"
+            />
+            <p className={s.name}>{friend.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
+  );
 }
 
- FriendList.propTypes = {
-    isOnline: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
-    name:PropTypes.string.isRequired,
-}
+FriendList.propTypes = {
+  isOnline: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+};
 
 export default FriendList;
--- src/components/Statistics/Statictics.js
+++ src/components/Statistics/Statictics.js
@@ -2,27 +2,23 @@ import s from './Statistics.module.css';
 import PropTypes from 'prop-types';
 console.log(s);
 
-function Statistics({ stats}) { 
-    return <div>
-        
-    <section className={ s.statistics}>
-        <h2 className={ s.title}>Upload stats</h2>
+function Statistics({ stats }) {
+  return (
+    <div>
+      <section className={s.statistics}>
+        <h2 className={s.title}>Upload stats</h2>
 
-        <ul className={ s.list}>
-            {stats.map(stat =>
-                <li key={stat.id } className={ s.item}>
-        
-                <span className={ s.label}>{ stat.label}</span>
-                <span className={s.percentage}>{ stat.percentage}%</span>
-              
-                    </li>
-                   
-                )}
-
-   
-  </ul>
-</section>
- </div>
+        <ul className={s.list}>
+          {stats.map(stat => (
+            <li key={stat.id} className={s.item}>
+              <span className={s.label}>{stat.label}</span>
+              <span className={s.percentage}>{stat.percentage}%</span>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
 }
 
 Statistics.propTypes = {
